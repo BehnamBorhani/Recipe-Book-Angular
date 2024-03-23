6085d9c97893ce6e6a6a4b6554476384ce5aab03
Behnam <behnamborhani95@gmail.com>
2024-03-23 13:02:56 +0330

    define the main app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  activeRoute: string = 'recipes';
-
-  onNavigate(route: string) {
-    this.activeRoute = route;
-  }
-}
+export class AppComponent {}
